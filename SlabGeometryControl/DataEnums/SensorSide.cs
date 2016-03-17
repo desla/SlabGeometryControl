a26b691ad976b58a25dbccda85a6e8e5f31a7c88
Денис <denxc@yandex.ru>
2016-03-17 16:26:17 +0700

    DataEnums перенес

---
+++ SlabGeometryControl/DataEnums/SensorSide.cs
@@ -0,0 +1,25 @@
+﻿namespace Alvasoft.DataEnums
+{
+    /// <summary>
+    /// Сторона рамки.
+    /// </summary>
+    public enum SensorSide
+    {
+        /// <summary>
+        /// Верхняя сторона рамки.
+        /// </summary>
+        TOP,
+        /// <summary>
+        /// Нижняя сторона рамки.
+        /// </summary>
+        BOTTOM,
+        /// <summary>
+        /// Левая сторона рамки.
+        /// </summary>
+        LEFT,
+        /// <summary>
+        /// Правая сторона рамки.
+        /// </summary>
+        RIGHT
+    }
+}
---
+++ SlabGeometryControl/DataEnums/SensorType.cs
@@ -0,0 +1,17 @@
+﻿namespace Alvasoft.DataEnums
+{
+    /// <summary>
+    /// Тип датчика.
+    /// </summary>
+    public enum SensorType
+    {
+        /// <summary>
+        /// Датчик положения.
+        /// </summary>
+        POSITION,
+        /// <summary>
+        /// Датчик расстояния.
+        /// </summary>
+        PROXIMITY
+    }
+}
