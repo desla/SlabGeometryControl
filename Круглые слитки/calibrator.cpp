ec4561b928e30905cd556b174243f357ebee1fec
Денис <denxc@yandex.ru>
2016-04-10 14:32:14 +0700

    калибратор - добавил правый датчик

--- Круглые слитки/calibrator.cpp
+++ Круглые слитки/calibrator.cpp
@@ -18,24 +18,26 @@ typedef long long ll;
 typedef unsigned long long ull;
 
 // начальные положения датчиков.
-double xa = -19.0, // верхний датчик.
-xb = -51.5, // левый датчик.
-xc = -30.0; // нижний датчик.
+double xa = -15.8, // верхний датчик.
+xb = -38.3, // левый датчик.
+xc = -19.0, // нижний датчик.
+xd = -62.5;   // правый датчик.
 
 // максимальный сдвиг датчиков влево и вправо, мм.
-double shiftLimit = 20;
+double shiftLimit = 5;
 
 // шаг сдвига, мм.
 double dx = 0.1;
 
 // показания датчиков.
-double va = 333.3333,
-vb = 298.6111,
-vc = 228.4722;
+double va = 332.6389, // верхний датчик.
+vb = 293.0556, // левый датчик.
+vc = 229.1667, // нижний датчик.
+vd = 336.1111;  // правый датчик.
 
 // расстояния между датчиками.
 double dac = 672.133,
-	   dbd = 735.327;
+dbd = 735.327;
 
 // диаметр болванки.
 double sourceDiameter = 110.453;
@@ -56,20 +58,43 @@ double getDiameter(point a, point b, point c) {
 	return (p1 * p2 * p3) / (2.0 * sqrt(p * (p - p1) * (p - p2) * (p - p3)));
 }
 
+void calcCircleCenter(point aA, point aB, point aC, point &center) {	
+	double x1 = aA.x, x2 = aB.x, x3 = aC.x,
+		y1 = aA.y, y2 = aB.y, y3 = aC.y;
+	double x12 = x1 - x2,
+		x23 = x2 - x3,
+		x31 = x3 - x1,
+		y12 = y1 - y2,
+		y23 = y2 - y3,
+		y31 = y3 - y1;
+	double z1 = x1 * x1 + y1 * y1,
+		z2 = x2 * x2 + y2 * y2,
+		z3 = x3 * x3 + y3 * y3;
+	double zx = y12 * z3 + y23 * z1 + y31 * z2,
+		zy = x12 * z3 + x23 * z1 + x31 * z2,
+		z = x12 * y31 - y12 * x31;
+	
+	center.x = -zx / (2.0 * z);
+	center.y = zy / (2.0 * z);
+}
+
 int main()
 {
-#if !defined(ONLINE_JUDGE)
-	freopen("input.txt", "r", stdin);
-	freopen("output.txt ", "w", stdout);
-#endif		
+	setlocale(LC_ALL, "Russian");
 
 	double error = DBL_MAX;
 	double resa, resb, resc;
 	double resDiameter;
+	point resCenter;
 
+	double stepsPerSensor = 2.0 * shiftLimit / dx;
+	double stepsCount = pow(stepsPerSensor, 3.0);
+
+	// вычисляем смещения верхнего, левого и нижнего датчиков.
+	int stepNumber = 0;
 	for (double ta = xa - shiftLimit; ta <= xa + shiftLimit; ta += dx) {
 		for (double tb = xb - shiftLimit; tb <= xb + shiftLimit; tb += dx) {
-			for (double tc = xc - shiftLimit; tc <= xc + shiftLimit; tc += dx) {				
+			for (double tc = xc - shiftLimit; tc <= xc + shiftLimit; tc += dx) {
 				point a, b, c;
 
 				a.x = ta;
@@ -89,16 +114,51 @@ int main()
 					resa = ta;
 					resb = tb;
 					resc = tc;
+					calcCircleCenter(a, b, c, resCenter);
 				}
-			}
+			}			
+		}
+
+		if (stepNumber % 10 == 0) {
+			system("cls");
+			printf("Завершено: %.1lf %%\n", 100 * stepNumber * stepsPerSensor * stepsPerSensor / stepsCount);
+		}
+
+		stepNumber++;
+	}
+
+	double minDifference = DBL_MAX;
+	double resd;
+
+	// вычисляем смещения правого датчика.
+	for (double td = xd - shiftLimit; td <= xd + shiftLimit; td += dx) {
+		point d;
+		d.x = dbd / 2.0 - vd;
+		d.y = td;
+
+		double distance = resCenter.distance(d);
+		double currentDifference = fabs(distance - sourceDiameter / 2.0);
+		if (currentDifference < minDifference) {
+			minDifference = currentDifference;
+			resd = td;
 		}
 	}
 
-	printf("Смещения датчиков:\n");
-	printf("Верхний: %.3lf\nЛевый: %.3lf\nНижний: %.3lf\n", resa, resb, resc);
-	printf("Высчитанный диаметр: %.3lf\n", resDiameter);
-	printf("Исходный диаметр: %.3lf\n", sourceDiameter);
-	printf("Ошибка: %.3lf\n", error);	
+	system("cls");
+	printf("Завершено: 100 %%\n");
+
+	FILE * output = fopen("output.txt", "w");
+	fprintf(output, "Смещения датчиков:\n");
+	fprintf(output, "Верхний: %.3lf\n", resa);
+	fprintf(output, "Левый: %.3lf\n", resb);
+	fprintf(output, "Нижний: %.3lf\n", resc);
+	fprintf(output, "Правый: %.3lf\n", resd);
+	fprintf(output, "\n");
+	fprintf(output, "Высчитанный диаметр: %.3lf\n", resDiameter);
+	fprintf(output, "Исходный диаметр: %.3lf\n", sourceDiameter);
+	fprintf(output, "Ошибка диаметра: %.3lf\n", error);
+	fprintf(output, "Ошибка правого датчика: %.3lf\n", minDifference);
+	fclose(output);
 
 	return 0;
 }
